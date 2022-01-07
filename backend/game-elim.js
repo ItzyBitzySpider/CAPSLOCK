eb844cfbc6e1d1efa62dc01bfb0e1cd13c06f315
jloh02 <36648707+jloh02@users.noreply.github.com>
2022-01-07 21:42:26 +0800

    Hotfix

--- backend/game-elim.js
+++ backend/game-elim.js
@@ -6,23 +6,6 @@ function createGame(io, socket, roomId, members, roomData) {
   roomData[roomId]["score"] = {};
   members.forEach((m) => (roomData[roomId]["score"][m] = 0));
   
-  socket.on("game elim submit", ({ roomId, word }) => {
-    console.log(socket.id + " submitted " + word);
-
-    const success = roomData[roomId]["wordlist"].delete(word);
-    if (success) {
-      console.log("Word accepted");
-      roomData[roomId]["score"][socket.id] += word.length;
-      const newWord = wordGen.generateNewWord();
-      io.to(roomId).emit("game elim update", {
-        user: socket.id,
-        word: word,
-        newWord: newWord,
-        scores: roomData[roomId]["score"],
-      });
-    }
-  });
-  
   io.to(roomId).emit(
     "game elim start",
     Array.from(roomData[roomId]["wordlist"])
