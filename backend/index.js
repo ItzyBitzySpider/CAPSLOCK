e016cf8dc367a2c34498cc493485e4c39b9ecb67
jloh02 <36648707+jloh02@users.noreply.github.com>
2022-01-07 21:33:29 +0800

    Open CORS

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.text());
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",//http://localhost:3000",
   },
 });
 
