cbd8a24a911a868f3f847ca4d1145acffe4c9adf
jloh02 <36648707+jloh02@users.noreply.github.com>
2022-01-07 14:56:12 +0800

    Print wordlist after generating

--- backend/game-elim.js
+++ backend/game-elim.js
@@ -3,6 +3,7 @@ const wordGen = require("./word-generation.js");
 //TODO combine these 2 into 1 function?
 function createGame(io, roomId, roomData) {
   roomData[roomId]["wordlist"] = new Set(wordGen.generateWordlist());
+  console.log(roomData[roomId]["wordlist"]);
   io.to(roomId).emit(
     "game elim start",
     Array.from(roomData[roomId]["wordlist"])
