c2bd5095979bb99583b343449cb1d0446ee073b4
jloh02 <36648707+jloh02@users.noreply.github.com>
2022-01-07 23:46:34 +0800

    Bug fix

--- backend/game-ad.js
+++ backend/game-ad.js
@@ -1,32 +1,36 @@
 const wordGen = require("./word-generation.js");
 
 function createGame(io, socket, roomId, members, roomData) {
-
   members.forEach((m) => {
     roomData[roomId][m] = {};
     roomData[roomId][m]["wordlist"] = [];
+    roomData[roomId][m]["timers"] = [];
     roomData[roomId][m]["lives"] = 3;
   });
 
   socket.on("game ad submit", ({ roomId, word }) => {
     console.log(socket.id + " submitted " + word);
 
-    //Defense
-    if(roomData[roomId][socket.id]["wordlist"].find((e)=>e===word)){
-      roomData[roomId][socket.id]["wordlist"].delete(word);
+    const idx = roomData[roomId][socket.id]["wordlist"].indexOf(word);
+    if (idx === -1) {
+      //Attack
+      for(var i=0;i<members.length;i++){
+        if(members[i] !== socket.id){
+          roomData[roomId][members[i]]["wordlist"].push(word);
+        }
+      }
+    } else {
+      //Defense
+      roomData[roomId][socket.id]["wordlist"].splice(idx, 1);
     }
 
-    io.to(roomId).emit("game ad update", {
-      roomData
-    });
-    
+    io.to(roomId).emit("game ad update", { roomData });
 
     const success = roomData[roomId]["wordlist"].delete(word);
     if (success) {
       console.log("Word accepted");
       roomData[roomId]["score"][socket.id] += word.length;
       const newWord = wordGen.generateNewWord();
-      
     }
   });
 }
