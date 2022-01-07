874049ba81f35b0cbf2697d20f863629059fc54e
jloh02 <36648707+jloh02@users.noreply.github.com>
2022-01-07 13:39:34 +0800

    Change localhost CORS

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const gameElim = require("./game-elim.js");
 const httpServer = require("http").createServer();
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: "http://localhost:8080",
+    origin: "http://localhost:3000",
   },
 });
 
